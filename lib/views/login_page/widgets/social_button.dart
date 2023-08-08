0b75621986dbab1c46202511386ca0333b3d56da
mariecrescentia1 <mariecrescentia00@gmail.com>
2023-08-08 16:43:17 +0300

    added

--- lib/views/login_page/widgets/social_button.dart
+++ lib/views/login_page/widgets/social_button.dart
@@ -18,40 +18,46 @@ class _SocialLoginButtonState extends State<SocialLoginButton> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            height: 50,
-            width: 190,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey.withOpacity(0.3)),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [                       
-                SvgPicture.asset("assets/icons/google-icon.svg",),
-                SizedBox(width: 10,),
-                Text('Google',style: TextStyle(fontWeight: FontWeight.w300,fontSize: 14, ))                      
-              ],
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 50,
+              width: 190,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.withOpacity(0.3)),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [                       
+                  SvgPicture.asset("assets/icons/google-icon.svg",),
+                  SizedBox(width: 10,),
+                  Text('Google',style: TextStyle(fontWeight: FontWeight.w300,fontSize: 14, ))                      
+                ],
+              ),
             ),
           ),
           SizedBox(width: 10),
-          Container(
-            height: 50,
-            width: 190,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey.withOpacity(0.3)),
-            ),
-                      
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [                       
-                SvgPicture.asset("assets/icons/facebook-2.svg",),
-                SizedBox(width: 10,),
-                Text('Facebook',style: TextStyle(fontWeight: FontWeight.w300,fontSize: 14, ))                      
-              ],
+          InkWell(
+            onTap: () {},
+            child: Container(
+              height: 50,
+              width: 190,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey.withOpacity(0.3)),
+              ),
+                        
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [                       
+                  SvgPicture.asset("assets/icons/facebook-2.svg",),
+                  SizedBox(width: 10,),
+                  Text('Facebook',style: TextStyle(fontWeight: FontWeight.w300,fontSize: 14, ))                      
+                ],
+              ),
             ),
           ),
                     
